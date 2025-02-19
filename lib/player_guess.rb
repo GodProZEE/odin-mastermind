1ca725a03d9bd2eb6271ad61661bdcbf232e8357
GodProZEE <100044640+GodProZEE@users.noreply.github.com>
2025-02-19 13:19:32 +0500

    Make the check loop 12 times, add temp_choice var
    temp_comp_choice fixes the issue with the computer choices being nil when
    changing them on purpose but then for the next iteration they remained nil.
    play_game function makes it play 12 times for 12 guesses by comp or player.

--- lib/player_guess.rb
+++ lib/player_guess.rb
@@ -14,16 +14,24 @@ module PlayerGuess
 
   def check_guess(computer_choice, guess_row, peg_area)
 
-  
+    peg_area = [0, 0, 0, 0]
     if computer_choice == guess_row
       peg_area = ["Red", "Red", "Red", "Red"]
     else
+      temp_comp_choice = []
+        computer_choice.each do |value|
+          temp_comp_choice.append(value)
+        end
       guess_row.each_with_index do |value, index|
-        if computer_choice.include?(value)
-          if computer_choice[index] == guess_row[index]
+        
+
+        p temp_comp_choice
+        if temp_comp_choice.include?(value)
+          if temp_comp_choice[index] == guess_row[index]
             peg_area[index] = "Red"
+            temp_comp_choice[index] = nil
           else
-            if computer_choice.count(value) >= guess_row.count(value) && computer_choice.count(value).zero? == false
+            if temp_comp_choice.count(value) >= guess_row.count(value) && temp_comp_choice.count(value).zero? == false
             puts "For the value #{value}, black is appended"
             peg_area[index] = "Black"
             end
--- main.rb
+++ main.rb
@@ -2,17 +2,41 @@ require_relative 'lib/board'
 
 board = Board.new
 colors = board.colors
-
-computer_choice = board.choose(board.colors)
 peg_area = board.peg_area
 guess_row = board.guess_row
 
-p computer_choice
-board.guess(guess_row, colors)
-guess_row = board.guess_row
 
 
 
-p board.check_guess(computer_choice, guess_row, peg_area)
 
+turn = ''
+
+
+def ask_player(turn)
+  puts "Do you want to choose the code or guess the code? Enter 'Choose' or 'Guess'"
+  turn = gets.chomp
+  return turn
+end
+
+
+def player_turn(board, colors, guess_row, peg_area)
+  computer_choice = board.choose(colors)
+  i = 0
+  loop do
+    guess_row = board.guess(guess_row, colors)
+    peg_area = board.check_guess(computer_choice, guess_row, peg_area)
+    i += 1
+    break if peg_area == ["Red", "Red", "Red", "Red"] or i >= 12
+  end
+end
+
+def play_game(board, turn, colors, guess_row, peg_area)
+  turn = ask_player(turn)
+  if turn == "Guess"
+    player_turn(board, colors, guess_row, peg_area)
+  end
+end
+
+play_game(board, turn, colors, guess_row, peg_area)
+    
 
